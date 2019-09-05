bec358ad4e4450a992cd7db74f6e6cf2b8b02186
Emelie <aakerse@wwu.edu>
2019-09-05 12:53:19 +0200

    test ping

--- network.go
+++ network.go
@@ -1,14 +1,46 @@
 package d7024e
 
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
 type Network struct {
 }
 
 func Listen(ip string, port int) {
 	// TODO
+	ln, err := net.Listen("tcp", ip+":"+strconv.Itoa(port))
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	data := make([]byte, 512)
+	n, err := conn.Read(data)
+	if err != nil {
+		panic(err)
+	}
+	s := string(data[:n])
+	print(s)
 }
 
 func (network *Network) SendPingMessage(contact *Contact) {
 	// TODO
+	conn, err := net.Dial("tcp", contact.Address)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(conn, "PING!")
 }
 
 func (network *Network) SendFindContactMessage(contact *Contact) {
